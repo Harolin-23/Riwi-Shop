9566c58e80f576c7bcf918e62dd587924876efe3
Harol <hchaverra26@gmail.com>
2024-10-16 16:52:25 -0500

    Add Header component and styles

---
+++ riwi-Shop/src/Components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.scss'
+
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <section className="contains-v-top">
+                <h1>Hola</h1>
+         
+            </section>
+        </header>
+    );
+};
+
+export default Header;
