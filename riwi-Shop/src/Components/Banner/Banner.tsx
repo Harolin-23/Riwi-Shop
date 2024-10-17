5fc06e7a8151a77545871f34e9e3ffae0288b295
Carlox <carlito1999@live.com>
2024-10-16 19:26:58 -0500

    banner de la imagen

--- riwi-Shop/src/Components/Banner/Banner.tsx
+++ riwi-Shop/src/Components/Banner/Banner.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import bannerImage from '../../assets/media/Assets 2 (package)/fondosPages/RIWI.ESPACIOS_0088.jpg';
+import React from "react";
+import BannerLanding from "../../assets/media/Assets 2 (package)/fondosPages/RIWI.ESPACIOS_0088.jpg";
+import "../Banner/Banner.scss";
 
 const Banner: React.FC = () => {
     return (
-        <div className="banner">
-            <div className="image-container">
-                <img src={bannerImage} alt="Banner Image" />
-            </div>
+      <section>
+        <div className="BannerLanding">
+          <div className="image-container">
+            <img src={BannerLanding} alt="Banner Image" />
+          </div>
+          <div className="info-mS-SHOP">
+            <h2>New Arrival</h2>
+            <h1>Discover Our <br /> New Collection</h1> {/* Corregido el <br> */}
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab id a, sint deserunt nam illum! Laborum
+            </p>
+            <a href="./redirects/shopRiwi/shop.html">
+              <button className="BTN-RED-SHOP">Buy now</button>
+            </a>
+          </div>
         </div>
+      </section>
     );
-};
+  };
+  
 
 export default Banner;
