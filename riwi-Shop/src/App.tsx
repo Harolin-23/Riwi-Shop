a3afd31b7079e7f0807ee996ee96ce31401ce2d5
Harol <hchaverra26@gmail.com>
2024-10-16 19:34:46 -0500

    Add Categories component and update styling,(Day 1 )

--- riwi-Shop/src/App.tsx
+++ riwi-Shop/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Header from './Components/Header/Header.tsx'
 import Banner from './Components/Banner/Banner.tsx'
-
+import Categories from './Components/categories/categories.tsx'
 
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
     <>
     <Header />
     <Banner />
+    <Categories categories={[]} />
     </>
   )
 }
